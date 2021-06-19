d1e0faa2e80447d23e5228b69ca402287d64119a
Erik Olson <erik@olson.host>
2021-06-19 21:32:27 +0000

    wip

---
+++ src/Twilio.php
@@ -0,0 +1,64 @@
+<?php 
+
+namespace Olsonhost\Twilio;
+
+// Perform common telco functions using Twilio
+
+// Send a text
+// Dial a number
+// Process webhook
+
+
+// These are low level functions that could be processed with Twilio, a dialogic card, or some other service or software
+
+class Twilio
+{
+
+
+	protected $token, $sid;
+
+	protected $ect; // This is the calling class containing ect event handlers
+
+	public function __construct($ect == false)
+	{
+
+	// Get configuration info
+	$this->token = '8574d87add64414a342f2ab159011e45';
+	$this->sid = 'AC03e2898812b9a0aaeed9c7d70824f326';
+
+
+	}
+
+	public function webhook($uri = false) {
+
+	// The webhook must invoke an event for ECT to handle
+
+	if ($ect == false) {
+
+		exit('TWILIO ERROR: ECT is undefined');
+
+		}
+
+	if ($uri == false) {
+		
+		// Invoke test event in calling class
+
+		$ect->test();
+
+		}
+
+
+	}
+
+	public function sendSMS($to, $from, $msg) {
+
+	}
+
+	public function dial($number, $callback) {
+
+	}
+ 
+
+
+
+}
